5b3710f5a9b72cd365992def5d6220e65c42f58a
Parsa Salamatipour <sa.parsa20@gmail.com>
2025-08-19 00:19:26 +0330

    feat: batch loading

--- auto_load.py
+++ auto_load.py
@@ -28,16 +28,18 @@ def add_default_card(card_number, balance):
 add_default_card("0000000000000000", 1000)
 add_default_card("1111111111111111", 1000)
 
-total_cards = 1000000
+total_cards = 10_000_000
+batch_size=100_000
 
-for i in tqdm(range(total_cards), desc="Inserting cards", unit="card"):
-    card_number = ''.join(random.choices('0123456789', k=16))
-    card_numbers.add(card_number)
+for i in tqdm(range(0, total_cards, batch_size), desc="Inserting cards", unit="card"):
+    batch = []
+    for j in range(batch_size):
+        card_number = ''.join(random.choices('0123456789', k=16))
+        if card_number not in card_numbers:
+            batch.append((card_number, random.randint(0, 10000)))
+        card_numbers.add(card_number)
     
-    cur.execute(
-        'INSERT INTO "Cards" (card_number, balance) VALUES (%s, %s)',
-        (card_number, random.randint(0, 10000))
-    )
+    cur.executemany('INSERT INTO "Cards" (card_number, balance) VALUES (%s, %s)',batch)
 
 conn.commit()
 
