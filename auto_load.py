d23e4abb662ec27a85db3a0a578fa927cf39e134
Parsa Salamatipour <sa.parsa20@gmail.com>
2025-08-30 22:52:47 +0330

    perf: use of string io for inserting faster

--- auto_load.py
+++ auto_load.py
@@ -1,8 +1,8 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
-import random
 from tqdm import tqdm
+import io
 
 load_dotenv()
 
@@ -15,12 +15,10 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-cur.execute('DELETE FROM "Cards"')
-
-card_numbers = set()
+cur.execute('TRUNCATE TABLE "Cards" RESTART IDENTITY CASCADE')
+cur.execute('TRUNCATE TABLE logs RESTART IDENTITY CASCADE')
 
 def add_default_card(card_number, balance):
-    card_numbers.add(card_number)
     cur.execute(
         'INSERT INTO "Cards" (card_number, balance) VALUES (%s, %s)',
         (card_number, balance)
@@ -28,18 +26,16 @@ def add_default_card(card_number, balance):
 add_default_card("0000000000000000", 1000)
 add_default_card("1111111111111111", 1000)
 
-total_cards = 10_000_000
-batch_size=100_000
+total_cards = 100_000_000
 
-for i in tqdm(range(0, total_cards, batch_size), desc="Inserting cards", unit="card"):
-    batch = []
-    for j in range(batch_size):
-        card_number = ''.join(random.choices('0123456789', k=16))
-        if card_number not in card_numbers:
-            batch.append((card_number, random.randint(0, 10000)))
-        card_numbers.add(card_number)
-    
-    cur.executemany('INSERT INTO "Cards" (card_number, balance) VALUES (%s, %s)',batch)
+buffer = io.StringIO()
+for i in tqdm(range(2, total_cards + 2), desc="Generating cards", unit="card"):
+    card_number = f"{i:016d}"
+    balance = 1000
+    buffer.write(f"{card_number}\t{balance}\n")
+
+buffer.seek(0)
+cur.copy_from(buffer, 'Cards', columns=('card_number', 'balance'), sep='\t')
 
 conn.commit()
 
