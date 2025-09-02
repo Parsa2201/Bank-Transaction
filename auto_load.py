f808d58e858d6715e5c98c08726018385c148c42
Parsa Salamatipour <sa.parsa20@gmail.com>
2025-09-02 21:30:25 +0330

    feat: change environment variable sources
    BREAKING CHANGE: .env is now replaced with environment configured in docker-compose.yaml

--- auto_load.py
+++ auto_load.py
@@ -1,17 +1,14 @@
 import psycopg2
-from dotenv import load_dotenv
 import os
 from tqdm import tqdm
 import io
 
-load_dotenv()
-
 conn = psycopg2.connect(
-    dbname=os.getenv("POSTGRES_DB"),
-    user=os.getenv("POSTGRES_USER"),
-    password=os.getenv("POSTGRES_PASSWORD"),
-    host=os.getenv("POSTGRES_HOST"),
-    port=os.getenv("POSTGRES_PORT")
+    dbname=os.getenv("DATABASE_NAME"),
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD"),
+    host=os.getenv("DATABASE_HOST"),
+    port=os.getenv("DATABASE_PORT")
 )
 cur = conn.cursor()
 
